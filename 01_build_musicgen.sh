d9a5ed121c076d28230246d71c2ce682f2d03955
Christoph J. Weber <web_chris@msn.com>
2023-07-25 10:38:58 +0200

    *

--- 01_build_musicgen.sh
+++ 01_build_musicgen.sh
@@ -2,7 +2,7 @@
 git clone -b v1.0 https://github.com/camenduru/audiocraft
 
 # Copy the dockerfile to the audiocraft folder
-cp dockerfile audiocraft/dockerfile
+cp Dockerfile audiocraft/Dockerfile
 
 # cd into the audiocraft folder
 cd audiocraft
