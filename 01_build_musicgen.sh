82161618b2c949330ab743ee5089c3adb664b3af
Christoph J. Weber <web_chris@msn.com>
2023-07-25 18:01:34 +0200

    *

--- 01_build_musicgen.sh
+++ 01_build_musicgen.sh
@@ -1,11 +1,2 @@
-# This script is used to load the docker image and run the container
-#git clone -b v1.0 https://github.com/camenduru/audiocraft
-
-# Copy the dockerfile to the audiocraft folder
-#cp Dockerfile audiocraft/Dockerfile
-
-# cd into the audiocraft folder
-#cd audiocraft
-
 # Build the docker image
 docker build -t musicgen .
