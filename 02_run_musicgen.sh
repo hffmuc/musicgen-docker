f8ae9dd28d693fd38dd34bc8d3637548423b0e3c
Christoph J. Weber <web_chris@msn.com>
2023-11-06 10:44:27 +0100

    *

--- 02_run_musicgen.sh
+++ 02_run_musicgen.sh
@@ -1,2 +1,3 @@
 # Run the docker container
-docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -p 7861:7860 --rm musicgen
+#docker run --gpus all --ipc=host --ulimit memlock=-1 --ulimit stack=67108864 -p 7861:7860 --rm musicgen
+docker run --gpus all -p 7863:7863 musicgen
